5e290a9331c48cc890cec97b07cf709d78f4a90c
azimilaw1 <azimilaw@yahoo.com>
2023-08-22 11:25:06 -0700

    changes to safi branch only

--- src/test/java/api/runner/TestClass.java
+++ src/test/java/api/runner/TestClass.java
@@ -4,6 +4,7 @@ public class TestClass {
 
 	public static void main(String[] args) {
       System.out.println("this class is to test in Jenkins");
+      System.out.println("This changes is only for safi branch");
   
 
 	}
